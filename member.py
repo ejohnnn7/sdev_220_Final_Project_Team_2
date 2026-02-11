65ef3cb0d6fd787ef8ca0743521ffb64df03c6ef
Marco Cerignoni <msanchescerigno@ivytech.edu>
2026-02-10 20:06:05 -0500

    Made member_id sequential and code determined

--- member.py
+++ member.py
@@ -1,7 +1,8 @@
 import sqlite3
 
 class Member:
-    def __init__(self, member_id, first_name, last_name, fines_due):
+    #Initialize member_id last as None to make it increment automatically instead of passing an arbitrary id
+    def __init__(self, first_name, last_name, fines_due, member_id=None):
         self.member_id = member_id
         self.first_name = first_name
         self.last_name = last_name
@@ -32,10 +33,15 @@ def add_member_to_db(member_obj):
     
     try:
         cursor.execute('''
-            INSERT INTO members (member_id, first_name, last_name, fines_due)
-            VALUES (?, ?, ?, ?)
-        ''', (member_obj.member_id, member_obj.first_name, member_obj.last_name, int(member_obj.fines_due)))
+            INSERT INTO members (first_name, last_name, fines_due)
+            VALUES (?, ?, ?)
+        ''', (member_obj.first_name, member_obj.last_name, int(member_obj.fines_due)))
+        
         conn.commit()
+        
+        #Make the member id be the unique row number
+        member_obj.member_id = cursor.lastrowid
+        
         print(f"✅ Added: {member_obj.first_name}")
     except sqlite3.IntegrityError:
         print(f"❌ Error: Book ID {member_obj.member_id} already exists.")
@@ -47,12 +53,12 @@ if __name__ == "__main__":
     # --- Execution Examples ---
     initialize_member_db()
 
-    # Create a new member instance
-    new_member = Member(1001, "Andrew", "Catlin", 0)
-
-    # Save it
-    add_member_to_db(new_member)
+    # #1. Create a new member instance
+    # new_member = Member("Andrew", "Catlin", 0)
+    # add_member_to_db(new_member)
+    
+    
 
 
 # The above will be used in adding a new member, it just needs a GUI to access it cleanly. 
-# The initialize_member_db will also be used to serach the DB.
+# The initialize_member_db will also be used to search the DB.
